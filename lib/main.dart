7d11f2afcf0dde2e027fc98e143e35ea1906a638
EllaHayashiIndro <ella.hayashi@indrorobotics.com>
2021-12-21 14:09:26 -0500

    adding form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'text_fields.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+
 void main() => runApp(MyApp());
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
@@ -9,6 +12,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+final _formKey = GlobalKey<FormState>();
+bool _enableBtn = false;
+TextEditingController emailController = TextEditingController();
+TextEditingController subjectController = TextEditingController();
+TextEditingController messageController = TextEditingController();
+
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -38,16 +49,91 @@ class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          color: Colors.blueAccent,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Home()));
-          },
-          child: Text('Send email'),
+      body: Form(
+        key: _formKey,
+        onChanged: (() {
+          setState(() {
+            _enableBtn = _formKey.currentState!.validate();
+          });
+        }),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              TextFields(
+                  controller: subjectController,
+                  name: "Subject",
+                  validator: ((value) {
+                    if (value!.isEmpty) {
+                      return 'Name is required';
+                    }
+                    return null;
+                  })),
+              TextFields(
+                  controller: emailController,
+                  name: "Email",
+                  validator: ((value) {
+                    if (value!.isEmpty) {
+                      return 'Email is required';
+                    } else if (!value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  })),
+              TextFields(
+                  controller: messageController,
+                  name: "Message",
+                  validator: ((value) {
+                    if (value!.isEmpty) {
+                      setState(() {
+                        _enableBtn = true;
+                      });
+                      return 'Message is required';
+                    }
+                    return null;
+                  }),
+                  maxLines: null,
+                  type: TextInputType.multiline),
+              Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                        MaterialStateProperty.resolveWith<Color>(
+                              (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed))
+                              return Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withOpacity(0.5);
+                            else if (states.contains(MaterialState.disabled))
+                              return Colors.grey;
+                            return Colors.blue; // Use the component's default.
+                          },
+                        ),
+                        shape:
+                        MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40.0),
+                            ))),
+                    onPressed: _enableBtn
+                        ? (() async {
+                      final Email email = Email(
+                        body: messageController.text,
+                        subject: subjectController.text,
+                        recipients: [emailController.text],
+                        isHTML: false,
+                      );
+                      await FlutterEmailSender.send(email);
+                    })
+                        : null,
+                    child: Text('Submit'),
+                  )),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
